b5aa726d802a7c271029548325989c92c93b0c34
farhadno <nooralahzadeh@gmail.com>
2016-10-22 13:01:19 +0200

    add some example to example page

--- app/components/examples.jsx
+++ app/components/examples.jsx
@@ -1,11 +1,21 @@
 var React=require('react');
+var {Link}=require('react-router');
 
 
 var Examples=(props)=>{
   return(
     <div>
-      <h3>Example component</h3>
-      <p>Welcome to Example page</p>
+      <h1 className="text-center">Example</h1>
+
+      <p>Here are a few example locations to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=Oslo'>Oslo</Link>
+        </li>
+        <li>
+          <Link to='/?location=Tehran'>Tehran</Link>
+        </li>
+      </ol>
     </div>
 
   );
