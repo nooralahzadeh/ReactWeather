e94e52187b566019b0a27325628d541bfa7b7324
farhadno <nooralahzadeh@gmail.com>
2016-10-23 10:05:59 +0200

    add error model

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,22 +1,19 @@
 var axios=require('axios');
 
 const OPEN_WEATHER_MAP_URL='http://api.openweathermap.org/data/2.5/weather?appid=a361a3738d29974970b705176892e829&units=imperial';
-module.exports={
-  getTemp: function(location){
-    var encodedLocation=encodeURIComponent(location);
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
     var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
 
-    return  axios.get(requestUrl).then(function(res){
-        if(res.data.cod && res.data.message){
-            throw new Error(res.data.message);
-
-        }else{
-          return res.data.main.temp;
-        }
-
-    },function(res){
+    return axios.get(requestUrl).then(function (res) {
+      if (res.data.cod && res.data.message) {
         throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
     });
-
   }
 }
